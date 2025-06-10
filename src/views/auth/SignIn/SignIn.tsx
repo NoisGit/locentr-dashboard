6d0564b5d7e2628e988f7d0f8005123ca261e7ff
Boris <boriznicolaz@gmail.com>
2025-06-10 12:02:28 -0400

    package-lock y logo mejorado

--- src/views/auth/SignIn/SignIn.tsx
+++ src/views/auth/SignIn/SignIn.tsx
@@ -25,7 +25,7 @@ export const SignInBase = ({
                     type="streamline"
                     mode={mode}
                     imgClass="mx-auto"
-                    logoWidth={60}
+                    logoWidth={200} 
                 />
             </div>
             <div className="mb-10">
