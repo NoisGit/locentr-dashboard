7ab15423596264eb1ed912ae618bece86dab94fa
Boris <108773158+NoisGit@users.noreply.github.com>
2026-04-29 18:55:31 -0400

    chore: configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 import dynamicImport from 'vite-plugin-dynamic-import'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), dynamicImport()],
+  base: mode === 'production' ? '/dashboard-base/' : '/',
   assetsInclude: ['**/*.md'],
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src'),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dev-api-residencial-dvhndhdqh8eeazgy.brazilsouth-01.azurewebsites.net',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
   build: {
-    outDir: 'build'
-  }
-})
+    outDir: 'build',
+  },
+}))
