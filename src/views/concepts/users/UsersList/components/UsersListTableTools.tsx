4583a780cb0c6baeb5c58226c226454418ad6730
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-14 07:36:04 -0400

    users: replace inherited table tools

--- src/views/concepts/users/UsersList/components/UsersListTableTools.tsx
+++ src/views/concepts/users/UsersList/components/UsersListTableTools.tsx
@@ -1,1 +1,30 @@
-export { default } from '@/views/concepts/customers/CustomerList/components/CustomersListTableTools'
+import { useCallback } from 'react'
+import DebouceInput from '@/components/shared/DebouceInput'
+import { TbSearch } from 'react-icons/tb'
+import cloneDeep from 'lodash/cloneDeep'
+import useUsersList from '../hooks/useUsersList'
+
+const UsersListTableTools = () => {
+  const { tableData, setTableData } = useUsersList()
+
+  const handleInputChange = useCallback((value: string) => {
+    const nextTableData = cloneDeep(tableData)
+    nextTableData.query = value ?? ''
+    nextTableData.pageIndex = 1
+    setTableData(nextTableData)
+  }, [tableData, setTableData])
+
+  return (
+    <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-2">
+      <DebouceInput
+        className="w-full md:w-80"
+        placeholder="Buscar usuarios..."
+        aria-label="Buscar usuarios"
+        suffix={<TbSearch className="text-lg" />}
+        onChange={(event) => handleInputChange(event.target.value)}
+      />
+    </div>
+  )
+}
+
+export default UsersListTableTools
