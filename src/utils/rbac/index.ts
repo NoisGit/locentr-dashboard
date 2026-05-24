69db4181496d662cfd4aa38e9a93e90faffb1340
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-24 07:40:35 -0400

    Fix duplicate RBAC exports

--- src/utils/rbac/index.ts
+++ src/utils/rbac/index.ts
@@ -2,7 +2,3 @@
 export * from './types'
 export * from './roleAliases'
 export * as RBAC from './rbacCore'
-
-// Re-export commonly used items for convenience
-export { Role, Permission, ROLE_PERMISSIONS } from './types'
-export type { AuthUser, PermissionCheckOptions, PermissionCheckResult } from './types'
