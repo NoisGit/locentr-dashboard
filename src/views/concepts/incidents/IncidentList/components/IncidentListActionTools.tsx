02fdbc24a5f438541fcf473c125942a767d2cae5
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-28 04:36:05 -0400

    Remove unused file

--- src/views/concepts/incidents/IncidentList/components/IncidentListActionTools.tsx
+++
@@ -1,9 +0,0 @@
-// src/views/concepts/incidents/IncidentList/components/IncidentListActionTools.tsx
-const IncidentListActionTools = () => {
-  return (
-    <div className="flex flex-col md:flex-row gap-3">
-    </div>
-  )
-}
-
-export default IncidentListActionTools
