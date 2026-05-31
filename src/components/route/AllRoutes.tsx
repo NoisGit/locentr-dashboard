e9fb1150b944266eca3e39da75f4d16835350a72
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-31 14:19:26 -0400

    Simplify route entry handling

--- src/components/route/AllRoutes.tsx
+++ src/components/route/AllRoutes.tsx
@@ -7,62 +7,69 @@ import { protectedRoutes, publicRoutes } from '@/configs/routes.config'
 import { useAuth } from '@/auth'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import type { LayoutType } from '@/@types/theme'
-import { getUserRoles } from '@/utils/authority'
 import { DASHBOARDS_PREFIX_PATH } from '@/constants/route.constant'
 
 interface ViewsProps {
-  pageContainerType?: 'default' | 'gutterless' | 'contained'
-  layout?: LayoutType
+    pageContainerType?: 'default' | 'gutterless' | 'contained'
+    layout?: LayoutType
 }
+
 type AllRoutesProps = ViewsProps
 
-const SUPER_ENTRY = `${DASHBOARDS_PREFIX_PATH}`
-const DEFAULT_ENTRY = `${DASHBOARDS_PREFIX_PATH}`
+const ENTRY_PATH = DASHBOARDS_PREFIX_PATH
+
+function toAuthChildPath(path: string) {
+    return path.replace(/^\/?auth\/?/, '')
+}
 
 const AllRoutes = (props: AllRoutesProps) => {
-  const { user } = useAuth()
+    const { user } = useAuth()
 
-  const roles = getUserRoles(user)
-  const isSuper = roles.has('SUPERADMIN')
-  const entryPath = isSuper ? SUPER_ENTRY : DEFAULT_ENTRY
+    return (
+        <Routes>
+            <Route path="/auth/*" element={<PublicRoute />}>
+                <Route index element={<Navigate replace to="sign-in" />} />
+                {publicRoutes.map((route) => (
+                    <Route
+                        key={route.key}
+                        path={toAuthChildPath(route.path)}
+                        element={
+                            <AppRoute
+                                routeKey={route.key}
+                                component={route.component}
+                                {...route.meta}
+                            />
+                        }
+                    />
+                ))}
+                <Route path="*" element={<Navigate replace to="sign-in" />} />
+            </Route>
 
-  const toAuthChildPath = (p: string) => p.replace(/^\/?auth\/?/, '')
+            <Route path="/" element={<ProtectedRoute />}>
+                <Route index element={<Navigate replace to={ENTRY_PATH} />} />
 
-  return (
-    <Routes>
-      <Route path="/auth/*" element={<PublicRoute />}>
-        <Route index element={<Navigate replace to="sign-in" />} />
-        {publicRoutes.map((route) => (
-          <Route
-            key={route.key}
-            path={toAuthChildPath(route.path)}
-            element={<AppRoute routeKey={route.key} component={route.component} {...route.meta} />}
-          />
-        ))}
-        <Route path="*" element={<Navigate replace to="sign-in" />} />
-      </Route>
+                {protectedRoutes.map((route) => (
+                    <Route
+                        key={route.key}
+                        path={route.path}
+                        element={
+                            <AuthorityGuard userAuthority={user} authority={route.authority}>
+                                <PageContainer {...props} {...route.meta}>
+                                    <AppRoute
+                                        routeKey={route.key}
+                                        component={route.component}
+                                        {...route.meta}
+                                    />
+                                </PageContainer>
+                            </AuthorityGuard>
+                        }
+                    />
+                ))}
 
-      <Route path="/" element={<ProtectedRoute />}>
-        <Route index element={<Navigate replace to={entryPath} />} />
-
-        {protectedRoutes.map((route, index) => (
-          <Route
-            key={route.key + index}
-            path={route.path}
-            element={
-              <AuthorityGuard userAuthority={user} authority={route.authority}>
-                <PageContainer {...props} {...route.meta}>
-                  <AppRoute routeKey={route.key} component={route.component} {...route.meta} />
-                </PageContainer>
-              </AuthorityGuard>
-            }
-          />
-        ))}
-
-        <Route path="*" element={<Navigate replace to={entryPath} />} />
-      </Route>
-    </Routes>
-  )
+                <Route path="*" element={<Navigate replace to={ENTRY_PATH} />} />
+            </Route>
+        </Routes>
+    )
 }
 
 export default AllRoutes
