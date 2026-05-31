e259b288cf11e2da95911834b7d8cccf3595f6d4
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-31 02:25:19 -0400

    Remove empty navigation configs

--- src/configs/navigation.config/index.ts
+++ src/configs/navigation.config/index.ts
@@ -1,12 +1,8 @@
 import dashboardsNavigationConfig from './dashboards.navigation.config'
-import uiComponentNavigationConfig from './ui-components.navigation.config'
-import authNavigationConfig from './auth.navigation.config'
 import type { NavigationTree } from '@/@types/navigation'
 
 const navigationConfig: NavigationTree[] = [
     ...dashboardsNavigationConfig,
-    ...uiComponentNavigationConfig,
-    ...authNavigationConfig,
 ]
 
 export default navigationConfig
