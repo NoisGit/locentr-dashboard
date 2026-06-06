c6138a65fd7bc767e1252724af4f16d030e0f56b
Boris <108773158+NoisGit@users.noreply.github.com>
2026-06-06 05:08:03 -0400

    chore: remove legacy authority from auth routes

--- src/configs/routes.config/authRoute.ts
+++ src/configs/routes.config/authRoute.ts
@@ -4,33 +4,28 @@ import type { Routes } from '@/@types/routes'
 const authRoute: Routes = [
     {
         key: 'signIn',
-        path: `/auth/sign-in`,
+        path: '/auth/sign-in',
         component: lazy(() => import('@/views/auth/SignIn')),
-        authority: [],
     },
     {
         key: 'forgotPassword',
-        path: `/auth/forgot-password`,
+        path: '/auth/forgot-password',
         component: lazy(() => import('@/views/auth/ForgotPassword')),
-        authority: [],
     },
     {
         key: 'resetPassword',
-        path: `/auth/reset-password`,
+        path: '/auth/reset-password',
         component: lazy(() => import('@/views/auth/ResetPassword')),
-        authority: [],
     },
     {
         key: 'otpVerification',
-        path: `/auth/otp-verification`,
+        path: '/auth/otp-verification',
         component: lazy(() => import('@/views/auth/OtpVerification')),
-        authority: [],
     },
     {
         key: 'companySelect',
         path: '/auth/company-select',
         component: lazy(() => import('@/views/auth/CompanySelect/CompanySelect')),
-        authority: [],
     },
 ]
 
