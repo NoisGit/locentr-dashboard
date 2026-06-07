7d887f28604098544334183885da4eb0c7df3bd0
Boris <108773158+NoisGit@users.noreply.github.com>
2026-06-07 09:03:11 -0400

    chore: support standard pagination in documents hook

--- src/views/documents/hooks/useDocuments.ts
+++ src/views/documents/hooks/useDocuments.ts
@@ -7,15 +7,22 @@ import {
 type UseDocumentsParams = {
     isSuperAdmin: boolean
     search: string
+    pageIndex: number
+    pageSize: number
 }
 
-export function useDocuments({ isSuperAdmin, search }: UseDocumentsParams) {
+export function useDocuments({
+    isSuperAdmin,
+    search,
+    pageIndex,
+    pageSize,
+}: UseDocumentsParams) {
     return useSWR(
-        ['documents:list', isSuperAdmin, search],
+        ['documents:list', isSuperAdmin, search, pageIndex, pageSize],
         () =>
             isSuperAdmin
-                ? apiListAllDocuments({ page: 1, search, size: 10 })
-                : apiListMyCompanyDocuments({ page: 1, search, size: 10 }),
+                ? apiListAllDocuments({ page: pageIndex, search, size: pageSize })
+                : apiListMyCompanyDocuments({ page: pageIndex, search, size: pageSize }),
         { revalidateOnFocus: false },
     )
 }
