1c9858d7ad78512b29b7551ac6285e74068fadd5
Boris <108773158+NoisGit@users.noreply.github.com>
2026-06-07 13:11:46 -0400

    chore: support standard pagination in notifications hook

--- src/views/notifications/hooks/useUnreadNotifications.ts
+++ src/views/notifications/hooks/useUnreadNotifications.ts
@@ -1,10 +1,18 @@
 import useSWR from 'swr'
 import { apiGetUnreadNotifications } from '@/services/NotificationsService'
 
-export function useUnreadNotifications() {
+type UseUnreadNotificationsParams = {
+    pageIndex: number
+    pageSize: number
+}
+
+export function useUnreadNotifications({
+    pageIndex,
+    pageSize,
+}: UseUnreadNotificationsParams) {
     return useSWR(
-        ['notifications:unread'],
-        () => apiGetUnreadNotifications({ page: 1, size: 10 }),
+        ['notifications:unread', pageIndex, pageSize],
+        () => apiGetUnreadNotifications({ page: pageIndex, size: pageSize }),
         { revalidateOnFocus: false },
     )
 }
