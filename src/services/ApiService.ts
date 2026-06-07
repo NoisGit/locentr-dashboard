d034f8bf7a114867c43ea37a54949d1e20f30729
Boris <108773158+NoisGit@users.noreply.github.com>
2026-06-07 02:49:30 -0400

    chore: remove temporary url normalization from ApiService

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,44 +1,20 @@
 import AxiosBase from './axios/AxiosBase'
-import type { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
-
-function normalizeUsersUrl(rawUrl: string) {
-  try {
-    const u = new URL(rawUrl, 'http://x')
-    const path = u.pathname
-    const q = u.search || ''
-    const h = u.hash || ''
-
-    if (/^\/api\/v1\/users\/me\/$/i.test(path)) return '/api/v1/users/me' + q + h
-
-    if (/^\/api\/v1\/users\/[^/]+\/$/i.test(path)) {
-      const trimmed = path.replace(/\/+$/, '')
-      return trimmed + q + h
-    }
-  } catch {
-    void 0
-  }
-
-  return rawUrl
-}
+import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
 
 const ApiService = {
-  fetchDataWithAxios<Response = unknown, Request = Record<string, unknown>>(
-    param: AxiosRequestConfig<Request>,
-  ) {
-    return new Promise<Response>((resolve, reject) => {
-      const rawUrl = String(param?.url ?? '')
-      const normalizedUrl = normalizeUsersUrl(rawUrl)
-      const finalCfg: AxiosRequestConfig<Request> = { ...param, url: normalizedUrl }
-
-      AxiosBase(finalCfg)
-        .then((response: AxiosResponse<Response>) => {
-          resolve(response.data)
+    fetchDataWithAxios<Response = unknown, Request = Record<string, unknown>>(
+        config: AxiosRequestConfig<Request>,
+    ) {
+        return new Promise<Response>((resolve, reject) => {
+            AxiosBase(config)
+                .then((response: AxiosResponse<Response>) => {
+                    resolve(response.data)
+                })
+                .catch((error: AxiosError) => {
+                    reject(error)
+                })
         })
-        .catch((errors: AxiosError) => {
-          reject(errors)
-        })
-    })
-  },
+    },
 }
 
 export default ApiService
