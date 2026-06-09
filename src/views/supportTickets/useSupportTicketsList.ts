6e109622f5f12e9e7b89f574cbb38fc7349ee150
Boris <108773158+NoisGit@users.noreply.github.com>
2026-06-08 21:16:54 -0400

    fix: complete support tickets pagination hook

--- src/views/supportTickets/useSupportTicketsList.ts
+++ src/views/supportTickets/useSupportTicketsList.ts
@@ -11,13 +11,3 @@ type UseSupportTicketsListParams = {
 export default function useSupportTicketsList({
     pageIndex,
     pageSize,
-    status,
-}: UseSupportTicketsListParams) {
-    const { data, error, isLoading, mutate } = useSWR(
-        ['support-tickets:list', pageIndex, pageSize, status],
-        () => apiGetAllSupportTickets({ page: pageIndex, size: pageSize, status }),
-        { revalidateOnFocus: false },
-    )
-
-    return {
-        tickets: data?.items ||
